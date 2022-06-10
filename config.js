2267fe52fdc9120e251afc23b5607321b68341e8
vanx2k6veo <81919180+vanx2k6veo@users.noreply.github.com>
2022-06-10 08:33:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    desc: 'Làm Ny A nhá 🫶',
+    btnYes: 'M Mơ À<33',
+    btnNo: 'Oke 🫶:3',
+    question:'Tại sao vậy <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'A Trêu e thế thoi chứ .. 😏 <33333333',
+    mess: '😏😏😏😏😏😏',
+    messDesc: 'Chúc Bạn Ngày mới vui vẻ ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/tao.ten.la.van.vann ' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
