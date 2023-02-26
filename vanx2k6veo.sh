4ad66b4d4d4fcd79daa513589c9e594038517a9a
vanx2k6veo <81919180+vanx2k6veo@users.noreply.github.com>
2023-02-26 07:44:38 +0700

    Update vanx2k6veo.sh

--- vanx2k6veo.sh
+++ vanx2k6veo.sh
@@ -4,10 +4,9 @@ termux-setup-storage
 apt update
 apt upgrade
 printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
-pkg install python
+pkg install php
 pip install --upgrade pip
-pip install requests
-curl -s https://raw.githubusercontent.com/vanx2k6veo/van-van/main/gop.php -o $home/usr/bin/vanx2k6veo
+curl -s https://raw.githubusercontent.com/vanx2k6veo/van-van/main/gop -o $home/usr/bin/vanx2k6veo
 chmod 777 $home/usr/bin/vanx2k6veo
 
 printf '\n\033[1;32m Gõ \033[1;33mvanx2k6veo \033[1;32mĐể Vào Tool <3\n\n'
