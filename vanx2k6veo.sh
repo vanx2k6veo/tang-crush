723a4d1b4090fd186ce3629d7d1c3f4f0117c6d6
vanx2k6veo <81919180+vanx2k6veo@users.noreply.github.com>
2023-02-26 07:33:12 +0700

    Add files via upload

---
+++ vanx2k6veo.sh
@@ -0,0 +1,13 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+termux-setup-storage
+apt update
+apt upgrade
+printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
+pkg install python
+pip install --upgrade pip
+pip install requests
+curl -s https://raw.githubusercontent.com/vanx2k6veo/van-van/main/gop.php -o $home/usr/bin/vanx2k6veo
+chmod 777 $home/usr/bin/vanx2k6veo
+
+printf '\n\033[1;32m Gõ \033[1;33mvanx2k6veo \033[1;32mĐể Vào Tool <3\n\n'
